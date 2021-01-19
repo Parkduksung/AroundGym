0b624d0726278ee5806d1c20ab475722b65ac0f1
Parkduksung <duksung1234@naver.com>
2021-01-19 23:58:48 +0900

    feature - IntroActivity 추가

---
+++ app/src/main/java/com/example/aroundgym/presenter/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aroundgym.presenter
+
+import android.os.Bundle
+import com.example.aroundgym.R
+import com.example.aroundgym.base.BaseActivity
+import com.example.aroundgym.databinding.ActivityIntroBinding
+
+class IntroActivity: BaseActivity<ActivityIntroBinding>(R.layout.activity_intro) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

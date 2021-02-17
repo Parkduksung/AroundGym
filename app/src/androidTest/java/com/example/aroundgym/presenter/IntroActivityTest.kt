b6626c2cba42fe243876a2e51b25f9653b5b18a1
Parkduksung <duksung1234@naver.com>
2021-02-18 00:14:04 +0900

    [intro]test - test runBlocking 으로 수정

--- app/src/androidTest/java/com/example/aroundgym/presenter/IntroActivityTest.kt
+++ app/src/androidTest/java/com/example/aroundgym/presenter/IntroActivityTest.kt
@@ -7,18 +7,25 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.aroundgym.R
-import kotlinx.coroutines.*
-import org.hamcrest.Matchers.not
-import org.junit.Assert.*
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.lang.Thread.sleep
 
 @RunWith(AndroidJUnit4::class)
 class IntroActivityTest {
 
+    @After
+    fun tearDown() = runBlocking {
+    }
+
+    @Before
+    fun setup() = runBlocking {
+    }
+
     @Test
-    fun should_not_show_icon_when_after_delay() {
+    fun should_not_show_icon_when_after_delay() = runBlocking<Unit> {
 
         ActivityScenario.launch(IntroActivity::class.java)
 

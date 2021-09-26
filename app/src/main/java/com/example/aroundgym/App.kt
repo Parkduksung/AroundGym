d38c2ad2837057c7218560d34b48d00b08bcdc57
Parkduksung <duksung1234@naver.com>
2021-09-26 19:12:19 +0900

    불필요한 부분들 다 모두 제거.

--- app/src/main/java/com/example/aroundgym/App.kt
+++ app/src/main/java/com/example/aroundgym/App.kt
@@ -1,12 +1,6 @@
 package com.example.aroundgym
 
 import android.app.Application
-import com.example.aroundgym.di.networkModule
-import com.example.aroundgym.di.repositoryModule
-import com.example.aroundgym.di.sourceModule
-import com.example.aroundgym.di.viewModelModule
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
 
 class App : Application() {
 
--- app/src/main/java/com/example/aroundgym/presenter/GymActivity.kt
+++ app/src/main/java/com/example/aroundgym/presenter/GymActivity.kt
@@ -1,18 +1,14 @@
 package com.example.aroundgym.presenter
 
 import android.os.Bundle
-import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
+import androidx.appcompat.app.AppCompatActivity
 import com.example.aroundgym.R
-import com.example.aroundgym.base.BaseActivity
-import com.example.aroundgym.databinding.ActivityMainBinding
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class GymActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class GymActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main)
     }
 
 }

6c26f32a6de7a029581c40f400ce118410802017
Parkduksung <duksung1234@naver.com>
2021-09-26 19:45:27 +0900

    [feature] BasePresenter , BaseView 추가.

---
+++ app/src/main/java/com/example/aroundgym/BasePresenter.kt
@@ -0,0 +1,7 @@
+package com.example.aroundgym
+
+interface BasePresenter {
+
+    fun start()
+
+}
---
+++ app/src/main/java/com/example/aroundgym/BaseView.kt
@@ -0,0 +1,5 @@
+package com.example.aroundgym
+
+interface BaseView<T> {
+    var presenter : T
+}

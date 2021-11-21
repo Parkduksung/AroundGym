a8229e5537877b6e1036904cbec7516a0d4ca78b
Parkduksung <duksung1234@naver.com>
2021-11-21 18:13:54 +0900

    [feature] MainActivity 수정.

--- app/src/main/java/com/example/aroundgym/MainActivity.kt
+++ app/src/main/java/com/example/aroundgym/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.aroundgym
 
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.EditText
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.aroundgym.data.model.Document
+import com.example.aroundgym.data.repo.KAKAORepositoryImpl
+import com.example.aroundgym.data.source.KAKAORemoteDataSourceImpl
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -11,9 +16,31 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val bookList by lazy { findViewById<ListView>(R.id.list_book) }
 
-    private val apiSearchBook by lazy { ApiSearchBook() }
+    private val etInputBook by lazy { findViewById<EditText>(R.id.et_input_book) }
 
-    private val list = mutableListOf<Document>()
+    private val mainViewStateListener = object : MainViewStateManager.MainViewState {
+        override fun search(list: List<Document>) {
+            runOnUiThread {
+                bookAdapter.addAll(list)
+            }
+        }
+
+        override fun error(message: String) {
+            runOnUiThread {
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private val mainManager by lazy {
+        MainViewStateManager(
+            KAKAORepositoryImpl.getInstance(
+                KAKAORemoteDataSourceImpl.getInstance()
+            )
+        ).apply {
+            setMainViewStateListener(mainViewStateListener)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,19 +49,10 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         bookList.run {
             adapter = bookAdapter
         }
-
-
-        val t = Thread {
-            apiSearchBook.search(bookName = "안드로이드")?.let { response ->
-                list.addAll(response.documents)
-            }
-        }
-
-        t.start()
-
-        t.join()
-
-        bookAdapter.addAll(list)
     }
 
+    fun search(view: View) {
+        bookAdapter.clear()
+        mainManager.search(etInputBook.text.toString())
+    }
 }
